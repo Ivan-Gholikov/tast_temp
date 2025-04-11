d198205745b5da6236e1a69bae3b114cc9eb2f01
Ivan-Gholikov <drpar@yandex.ru>
2025-04-11 10:10:56 +0300

    Add files via upload

---
+++ task_temperatura.py
@@ -0,0 +1,10 @@
+temp = 35
+
+if temp in range(-10, 1):
+     print ('Холодно')
+elif temp in range(1 , 26):
+    print("Тепло")
+elif temp in range(26,36):
+    print("Жарко")
+else:
+    print('Температура вне нормы')
